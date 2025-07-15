8420ba573d97f07805f34e0f098a8b890831dceb
AI-ML-Prowork <pythoncfc@gmail.com>
2025-07-15 15:27:35 +0530

    added changes

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -17,18 +17,18 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     # for the super admin(saas login)
-    path("admin", admin.site.urls),
-    path('saas/login/', TokenObtainPairView.as_view(), name='token_obtain_pair-saas-login'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('saas/users', CustomUserListView.as_view(), name='custom-user-list'),
-    path('saas/domains', DomainListView.as_view(), name='domain-list'),
-    path('saas/domains/update/<int:pk>', update_domain, name='domain-update'),
-    path('saas/admins', TenantListView.as_view(), name='tenant-list'),
-    path('saas/dashboard', dashboard, name='dashboard'),
-    path('saas/login', saas_login, name='saas_login'),
-    path('saas/logout', saas_logout, name ='saas_logout'),
+    path("backend/admin", admin.site.urls),
+    path('backend/saas/login/', TokenObtainPairView.as_view(), name='token_obtain_pair-saas-login'),
+    path('backend/api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('backend/saas/users', CustomUserListView.as_view(), name='custom-user-list'),
+    path('backend/saas/domains', DomainListView.as_view(), name='domain-list'),
+    path('backend/saas/domains/update/<int:pk>', update_domain, name='domain-update'),
+    path('backend/saas/admins', TenantListView.as_view(), name='tenant-list'),
+    path('backend/saas/dashboard', dashboard, name='dashboard'),
+    path('backend/saas/login', saas_login, name='saas_login'),
+    path('backend/saas/logout', saas_logout, name ='saas_logout'),
 
-    path('saas/theme-settings', theme_setting, name='theme_setting'),
+    path('backend/saas/theme-settings', theme_setting, name='theme_setting'),
 
     
     # get domain
@@ -36,8 +36,8 @@ urlpatterns = [
 
     path("backend/tenant/register", TenantRegister.as_view(), name="tenant-register"),
     path("backend/check-tenant", CheckTenant.as_view(), name="check-tenant"),
-    re_path(r"^swagger(?P<format>\.json|\.yaml)$",schema_view.without_ui(cache_timeout=0),name="schema-json"),
-    path("docs/",schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui"),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    re_path(r"^backend/swagger(?P<format>\.json|\.yaml)$",schema_view.without_ui(cache_timeout=0),name="schema-json"),
+    path("backend/docs/",schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui"),
+    path("backend/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 
 ]+website_urls
