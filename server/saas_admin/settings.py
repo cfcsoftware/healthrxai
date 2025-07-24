336dd86a8461456a29b5c955553f8b2b8f9a1dd5
AI-ML-Prowork <pythoncfc@gmail.com>
2025-07-24 20:23:16 +0530

    changes in settigs

--- server/saas_admin/settings.py
+++ server/saas_admin/settings.py
@@ -297,6 +297,9 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "http://www.healthrxai.com",
+    "http://*.healthrxai.com",
+    
 ]
 
 
